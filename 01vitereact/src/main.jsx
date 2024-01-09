2a352d2fd20ba40411d0a4d0d9dd0fd4ac4af251
mohitjha0806 <mohitjha95844@gmail.com>
2024-01-09 23:22:38 +0530

    done All  This Project

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App !</h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: "a",
+//     props: {
+//       href: "https://google.com",
+//       target: "_blank",
+//     },
+//     children: "Click me to visit Google",
+//   };
+
+  const anotherEliment = (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+  )
+
+  const anotherUser = "chai aur rect"
+
+  const reactElement = React.createElement(
+    'a', 
+    {href: 'https://google.com', target:'_blank'},'Click me to visit google',
+    anotherUser
+  )
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    reactElement
 )
