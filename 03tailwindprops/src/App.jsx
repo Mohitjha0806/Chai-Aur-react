a3d09071b4a25d25c0e162a275ee73f6bdfca08d
Mohitjha0806 <mohitjha95844@gmail.comm>
2024-03-19 23:54:53 +0530

    25:00 props with tailwind css

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+import Card from "./componants/Card";
+
+function App() {
+  const [count, setCount] = useState(0);
+  let myObj = {
+    username: "Mohit",
+    age: "23",
+  };
+  let newArr = [1, 2, 3, 4];
+
+  return (
+    <>
+      <h1 className="bg-green-400 p-4 rounded-xl mb-4">tailwint test</h1>
+      {/* <Card channel="chaiaurcode" someObj={myObj} someArr={newArr} /> */}
+      <Card username="Jha"/>
+      <Card username="Mohit Jha"/>
+    </>
+  );
+}
+
+export default App;
