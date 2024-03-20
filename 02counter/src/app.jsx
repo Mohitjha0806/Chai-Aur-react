52decc6e244a15cbda86a381b7487c6a6d5c4e80
Mohitjha0806 <mohitjha95844@gmail.comm>
2024-03-20 18:43:43 +0530

    Changes to be committed: modified:   02counter/src/app.jsx

--- 02counter/src/app.jsx
+++ 02counter/src/app.jsx
@@ -12,18 +12,14 @@ export function App() {
     setCounter(counter + 1);
     setCounter(counter + 1);
 
-    isme ☝️ ye code baar baar likhni par bhi same ek baar hi kam karega code agar value change karni hai toh last result ko leke + 1 karn ahai hai jo neeche 👇  bataya hai 
+    //NOTE :- isme ☝️ ye code baar baar likhni par bhi same ek baar hi kam karega code agar value change karni hai toh last result ko leke + 1 karn ahai hai jo neeche 👇  bataya hai 
 
-
     setCounter((prevCounter) => prevCounter + 1);
     setCounter((prevCounter) => prevCounter + 1);
     setCounter((prevCounter) => prevCounter + 1);
     setCounter((prevCounter) => prevCounter + 1);
     setCounter((prevCounter) => prevCounter + 1);
-
-    // 
-  
-  };
+};
 
   const removeValue = () => {
     if (counter > 0) {
