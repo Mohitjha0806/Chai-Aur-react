ee016e5eb1a26391ec0000f4b93cf31afc011ecf
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2024-04-01 00:19:49 +0530

    Fix ERROR done

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -1,41 +1,44 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import { ThemeProvide } from "./Context/theme";
-import ThemeBtn from "./components/ThemeBtn";
-import Card from "./components/Card";
+
+import { useEffect, useState } from 'react'
+import './App.css'
+import { ThemeProvider } from './Context/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
-  const [themeMode, setThemeMode] = useState("light");
+  const [themeMode, setThemeMode] = useState("light")
 
   const lightTheme = () => {
-    setThemeMode("light");
-  };
-  const darkTheme = () => {
-    setThemeMode("dark");
-  };
+    setThemeMode("light")
+  }
 
-  // actual theme change
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // actual change in theme
 
   useEffect(() => {
-    document.querySelector("html").classList.remove("dark", "light");
-    document.querySelector("html").classList.add(themeMode);
-  }, [themeMode]);
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
 
   return (
-    <ThemeProvide value={{ themeMode, lightTheme, darkTheme }}>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
       <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            <ThemeBtn />
-          </div>
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
 
-          <div className="w-full max-w-sm mx-auto">
-            <Card />
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
           </div>
-        </div>
       </div>
-    </ThemeProvide>
-  );
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
