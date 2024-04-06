c4a3c02ac56503e021b9c9c283cdfe6f9e4ade24
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2024-04-06 15:21:39 +0530

    No Error Down ALL solve error of todoSlice.js

--- reduxToolkitTodo/src/feature/todo/todoSlice.js
+++ reduxToolkitTodo/src/feature/todo/todoSlice.js
@@ -1,26 +1,28 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import {createSlice, nanoid } from '@reduxjs/toolkit';
 
-const intialState = {
-  todo: [{ id: 1, text: "Hello World" }],
-};
+const initialState = {
+    todos: [{id: 1, text: "Hello world"}]
+}
+
+
 
 export const todoSlice = createSlice({
-  name: "todo",
-  intialState,
-  reducers: {
-    addTodo: (state, action) => {
-      const todo = {
-        id: nanoid(),
-        text: action.payload,
-      };
-      state.todos.push(todo);
-    },
-    removeTodo: (state, action) => {
-      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
-    },
-  },
-});
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(), 
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
+        },
+    }
+})
 
-export const { addTodo, removeTodo } = todoSlice.actions;
+export const {addTodo, removeTodo} = todoSlice.actions
 
-export default todoSlice.reducer;
+export default todoSlice.reducer
